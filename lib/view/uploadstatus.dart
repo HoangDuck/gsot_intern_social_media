5e7bad145921c0431c8eafa8800f59f034aab617
HoangDuck <hoanghuuduc22@gmail.com>
2022-01-12 15:07:27 +0700

    Commit picker image

--- lib/view/uploadstatus.dart
+++ lib/view/uploadstatus.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:social_media/converter/data_converter.dart';
 import 'package:social_media/model/user.dart';
+import 'package:image_picker/image_picker.dart';
 class UploadStatus extends StatefulWidget {
   const UploadStatus({Key? key}) : super(key: key);
   @override
@@ -9,12 +12,20 @@ class UploadStatus extends StatefulWidget {
 }
 
 class _UploadStatusState extends State<UploadStatus> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? _imageFilePicker;
+
+  set _imageFile(XFile? value) {
+    _imageFilePicker = value;
+  }
+  // Pick an image
   @override
   Widget build(BuildContext context) {
     DataConvert dataConvert=Provider.of<DataConvert>(context);
     User user=dataConvert.currentUser;
     return Material(
       child: Container(
+        color: Colors.blueAccent,
         padding: const EdgeInsets.all(10),
         child: Container(
           decoration: const ShapeDecoration(
@@ -34,64 +45,127 @@ class _UploadStatusState extends State<UploadStatus> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                height: 65.0,
-                child: Row(
-                  children: [
-                    const SizedBox(width: 10,),
-                    SizedBox(
-                      height: 55,
-                      width: 55,
-                      child: CircleAvatar(
-                        radius: 30.0,
-                        backgroundImage:
-                        NetworkImage(user.picture.toString()),
-                        backgroundColor: Colors.transparent,
+              Container(
+                padding: EdgeInsets.all(5),
+                child: SizedBox(
+                  height: 65.0,
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 10,),
+                      SizedBox(
+                        height: 55,
+                        width: 55,
+                        child: CircleAvatar(
+                          radius: 30.0,
+                          backgroundImage:
+                          NetworkImage(user.picture.toString()),
+                          backgroundColor: Colors.transparent,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 10,),
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(user.name.toString(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                          Text(user.nickname.toString(), style: TextStyle(fontSize: 15),)
-                        ],
+                      const SizedBox(width: 10,),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(user.name.toString(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                            Text(user.nickname.toString(), style: TextStyle(fontSize: 15),)
+                          ],
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        child: const Icon(Icons.cancel),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.centerRight,
+                          child: IconButton(
+                            onPressed: (){
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(Icons.cancel),
+                          ),
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 10,)
-                  ],
-                ),
-              ),
-              Expanded(child: Container(
-                  padding: const EdgeInsets.all(15),
-                  child: Text("data.content.toString()",style: const TextStyle(fontSize: 20, color: Colors.black54),),
-                ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height*0.5,
-                child: Center(
-                  child: Image.network(
-                    //data.image.toString(),
-                    "https://cdn.baogiaothong.vn/upload/images/2020-4/article_social_image/2020-12-15/noel-1608021530-width1200height630-auto-crop-1608021622-width1200height630-1608021625-width1200height630.jpg",
-                    errorBuilder: (context,error,stacktrace){
-                      return Container();
-                    },
+                      const SizedBox(width: 10,)
+                    ],
                   ),
                 ),
               ),
+              TextFormField(
+                initialValue: 'What do you think?',
+                cursorColor: Colors.black,
+                maxLines: 5,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    contentPadding:
+                    EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+                    hintText: "Tell your thinking here"),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  child: _previewImages(),
+                  ),
+                ),
+              Divider(
+                color: Colors.black,
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                      onPressed: (){
+                        _onImageButtonPressed(ImageSource.gallery, context: context);
+                      },
+                      icon: Icon(Icons.image),
+                  ),
+                  IconButton(
+                    onPressed: (){
+                    },
+                    icon: Icon(Icons.send),
+                  ),
+                ],
+              )
             ],
           ),
         ),
       ),
     );
+  }
+  void _onImageButtonPressed(ImageSource source, {BuildContext? context}) async {
+    final pickedFile = await _picker.pickImage(source: source,);
+    setState(() {
+      _imageFile = pickedFile;
+      },
+    );
+  }
+  Widget _previewImages() {
+    if (_imageFilePicker != null) {
+      return Semantics(
+                label: "image_picker_example_picked_image",
+                child: kIsWeb
+                    ? Image.network(_imageFilePicker.toString())
+                    : Image.file(File(_imageFilePicker!.path)),
+              );
+    } else if (_imageFilePicker != null) {
+      return Text(
+        'Pick image error: $_imageFilePicker',
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return const Text(
+        '',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+
   }
 }
